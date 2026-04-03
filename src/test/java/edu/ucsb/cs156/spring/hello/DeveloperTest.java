239c7cc83cc16bce8d7f03232f1dc59b7159246b
Taedon Reth <taedonreth@gmail.com>
2026-04-02 18:14:29 -0700

    tr - updated with actual github id

--- src/test/java/edu/ucsb/cs156/spring/hello/DeveloperTest.java
+++ src/test/java/edu/ucsb/cs156/spring/hello/DeveloperTest.java
@@ -26,5 +26,9 @@ public class DeveloperTest {
 
     // TODO: Add additional tests as needed to get to 100% jacoco line coverage, and
     // 100% mutation coverage (all mutants timed out or killed)
+    @Test
+    public void getGithubId_returns_correct_githubId() {
+        assertEquals("taedonreth", Developer.getGithubId());
+    }
 
 }
