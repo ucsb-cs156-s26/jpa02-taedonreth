c055be1ce57a209c019a70136f6dcb10d33d3757
Taedon Reth <taedonreth@gmail.com>
2026-04-03 22:08:28 -0700

    tr - fix testing gap in equals, hashcode, and toString

--- src/test/java/edu/ucsb/cs156/spring/hello/TeamTest.java
+++ src/test/java/edu/ucsb/cs156/spring/hello/TeamTest.java
@@ -1,7 +1,8 @@
 package edu.ucsb.cs156.spring.hello;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -23,4 +24,47 @@ public class TeamTest {
     // TODO: Add additional tests as needed to get to 100% jacoco line coverage, and
     // 100% mutation coverage (all mutants timed out or killed)
 
+    @Test
+    public void toString_returns_correct_string() {
+        assertEquals("Team(name=test-team, members=[])", team.toString());
+    }
+
+    @Test
+    public void equals_with_same_objects_is_true() {
+        team = new Team();
+
+        assertTrue(team.equals(team));
+    }
+
+    @Test
+    public void equals_with_diff_objects_is_false() {
+        team = new Team();
+        int x = 5;
+
+        assertFalse(team.equals(x));
+    }
+    
+    @Test
+   public void equals_with_same_fields_is_true() {
+        Team team1 = new Team("test-team");
+        Team team2 = new Team("test-team");
+
+        assertTrue(team1.equals(team2));
+    }
+
+    @Test
+    public void equals_with_diff_fields_is_false() {
+        Team team1 = new Team("test-team1");
+        Team team2 = new Team("test-team2");
+
+        assertFalse(team1.equals(team2));
+    }
+
+    @Test
+    public void hashCode_returns_true() {
+        team = new Team();
+        int result = team.hashCode();
+        int expectedResult = 1;
+        assertEquals(expectedResult, result);
+    }
 }
